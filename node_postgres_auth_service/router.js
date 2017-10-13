2c07cc23b1464af75510cae73fe15788b879083b
Bilger Yahov <bilger@studyportals.com>
2017-10-13 12:44:28 +0200

    Add JWT token authentication route.

--- node_postgres_auth_service/router.js
+++ node_postgres_auth_service/router.js
@@ -1,7 +1,7 @@
 /**
  * @file router.js
  *
- * Router of the API.
+ * Router of the Authentication API.
  *
  * @author Bilger Yahov <bayahov1@gmail.com>
  * @version 1.0.0
@@ -26,6 +26,11 @@ router
 router
 	.route('/users/login')
 	.post(usersController.login);
+
+// Authenticate an existing user using a JWT token.
+router
+	.route('/users/authenticate')
+	.post(usersController.authenticate());
 
 // Export the router.
 module.exports = router;
