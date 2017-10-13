e24cc1aec325f06242fd039f298d1bbf57cab2d6
Bilger Yahov <bilger@studyportals.com>
2017-10-13 12:01:50 +0200

    Implement register function.

--- node_postgres_auth_service/users.controller.js
+++ node_postgres_auth_service/users.controller.js
@@ -11,6 +11,10 @@
 
 'use strict';
 
+// Dependencies for proper functioning of the service.
+const bcrypt = require('bcrypt-nodejs');
+const jwt = require('jsonwebtoken');
+
 // Set up all the configurations.
 const promise = require('bluebird');
 const options = {
@@ -25,12 +29,65 @@ const connectionConfig = {
 	password: '123456'
 };
 const dbInstance = pgPromise(connectionConfig);
+const tableName = 'users';
 
 module.exports = {
 
-	register(){
+	/**
+	 * Registers a single user.
+	 *
+	 * @param req
+	 * @param res
+	 *
+	 * @return void
+	 */
 
+	register(req, res){
 
+		// Check if all fields are present.
+		if(!req.body.email || !req.body.first_name ||
+			!req.body.last_name || !req.body.pass){
+
+			res
+				.status(400)
+				.json({
+					message: 'Email, first name, last name and password are required!'
+				});
+			return;
+		}
+
+		// All fields are present, go ahead!
+		console.log('Registering a new user!');
+
+		let rawPass = req.body.pass;
+
+		// All the attributes are named the same way as in the database.
+		let email       = req.body.email;
+		let first_name  = req.body.first_name;
+		let last_name   = req.body.last_name;
+		let pass        = bcrypt.hashSync(rawPass, bcrypt.genSaltSync(10));
+
+		let query =  `INSERT INTO ${tableName} (email, first_name, last_name, pass)`;
+			query += ` VALUES ('${email}', '${first_name}', '${last_name}', '${pass}');`;
+
+		dbInstance.none(query)
+			.then(function () {
+
+				console.log('User created!');
+				res
+					.status(201)
+					.json({
+						message: 'User created!'
+					});
+			})
+			.catch(function (error) {
+
+				// Log the error and return it as a message as well.
+				console.log(error);
+				res
+					.status(400)
+					.json(error);
+			});
 	},
 
 	login(){
